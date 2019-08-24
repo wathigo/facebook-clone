10702b916eb848775a010e0cf4d2770f8e2b16b5
wathigo <wathigosimon@gmail.com>
2019-08-24 16:52:23 -0400

    Update tests to use factory bot instead of fixtures

--- facebook/app/models/user.rb
+++ facebook/app/models/user.rb
@@ -9,4 +9,9 @@ class User < ApplicationRecord
   validates :email,   presence: true,
                        format: { with: VALID_EMAIL_REGEX }
   before_save   :downcase_email
+
+  private
+    def downcase_email
+      email.downcase!
+    end
 end
