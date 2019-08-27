bf4f8a84cc1b226e0caa6e26e5b7d1abe107b61a
wathigo <wathigosimon@gmail.com>
2019-08-27 01:22:37 -0400

    Add Post models, modify views

---
+++ facebook/db/migrate/20190823044456_add_username_to_users.rb
@@ -0,0 +1,4 @@
+class AddUsernameToUsers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
---
+++ facebook/spec/factories/posts.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :post do
+    title { "MyText" }
+    content { "MyText" }
+    user { nil }
+  end
+end
