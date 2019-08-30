5b57dc4aef512242f0874ef8e7ecbd92b1decb24
wathigo <wathigosimon@gmail.com>
2019-08-30 07:31:50 -0400

    Add test fot user#index controller

--- facebook/spec/controllers/users_controller_spec.rb
+++ facebook/spec/controllers/users_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:michael) { FactoryBot.create(:user) }
   describe '#show' do
     before do
       @request.env["devise.mapping"] = Devise.mappings[:user]
     end
-    let(:michael) { FactoryBot.create(:user) }
 
     it 'It should not render :show when user in not logged in' do
       get :show, params: { id: michael.id }
@@ -16,6 +16,22 @@ RSpec.describe UsersController, type: :controller do
       sign_in michael
       get :show, params: { id: michael.id }
       expect(response).to render_template(:show)
+    end
+  end
+
+  describe '#index' do
+    before do
+      sign_in michael
+    end
+
+    it "Successfully sends a get request to user#index route" do
+      get :index
+      expect(response.status).to eql(200)
+    end
+
+    it "Renders user index page if user is signed in" do
+      get :index
+      expect(response).to render_template(:index)
     end
   end
 end
