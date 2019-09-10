1f783668db4c411b9e09d6e36c4fdf32617e9c87
wathigo <wathigosimon@gmail.com>
2019-09-10 13:46:28 -0400

    Add friendship models and associations

--- fb/app/models/user.rb
+++ fb/app/models/user.rb
@@ -7,7 +7,16 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_one_attached :avatar
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship",
+                                 :foreign_key => "friend_id",
+                                 :dependent => :destroy
   scope :all_except, ->(user) { where.not(id: user) }
+  scope :confirmed_friendhips,     -> { friendships.where(user_id: user_id).where(confirmed: true) }
+ scope :confirmed_inverse_friendships,  -> { friendships.where(friend_id: user_id).where(confirmed: true) }
+
+ # A union of the aforementioned scopes
+ scope :friends, -> { union_scope(confirmed_friendhips, confirmed_inverse_friendships)}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -24,4 +33,5 @@ class User < ApplicationRecord
   def downcase_email
     email.downcase!
   end
+
 end
