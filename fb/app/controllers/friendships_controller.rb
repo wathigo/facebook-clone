169185b5d2c6f48f3d9b648cd0cc83ae48df8e1e
wathigo <wathigosimon@gmail.com>
2019-09-27 05:03:03 -0400

    Fix a bug in the friendship controller

--- fb/app/controllers/friendships_controller.rb
+++ fb/app/controllers/friendships_controller.rb
@@ -26,7 +26,7 @@ class FriendshipsController < ApplicationController
     else
       @friendship = current_user.inverse_friendships.find_by_id(params[:friendship_id])
     end
-    @friendship ||= user.pending_inverse_friendships.find { |friendship| friendship.user = current_user }
+    @friendship ||= current_user.pending_friendships.find { |friendship| friendship.friend = user }
     redirect_back(fallback_location: root_path) if @friendship.delete
   end
 
