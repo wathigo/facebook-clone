fb21c859cca376496d658b500107f2b69dcabdde
wathigo <wathigosimon@gmail.com>
2019-10-09 11:44:40 -0400

    Clean up code

--- fb/config/application.rb
+++ fb/config/application.rb
@@ -12,7 +12,6 @@ module Facebook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.factory_bot.definition_file_paths = ['spec/factories']
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
